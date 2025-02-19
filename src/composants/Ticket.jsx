b47dab78d33317d0d3a526da2f8dd38408bba784
FragRappy <breton.charles@live.fr>
2025-02-19 22:18:19 +0100

    console log supprimer

--- src/composants/Ticket.jsx
+++ src/composants/Ticket.jsx
@@ -29,7 +29,6 @@ export default function Ticket() {
       );
       const data = await response.json();
       setTicket(data);
-      console.log(data);
     } catch (error) {
       setError(error.message);
     } finally {
@@ -122,7 +121,6 @@ export default function Ticket() {
       );
 
       const data = await response.json();
-      console.log(data.records);
       setCommentaires(data.records);
     } catch (error) {
       console.error("Erreur lors de la récupération des commentaires:", error);
