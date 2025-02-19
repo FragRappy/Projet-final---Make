e36db423caa172e1e40bbec48c438f9d408c0c94
FragRappy <breton.charles@live.fr>
2025-02-19 22:26:07 +0100

    dernier console log

--- src/composants/Ticket.jsx
+++ src/composants/Ticket.jsx
@@ -73,7 +73,6 @@ export default function Ticket() {
       );
       const data = await response.json();
       setClient(data);
-      console.log(data);
     } catch (error) {
       setError(error.message);
     } finally {
